f3310e13038c8c4cdf498845492abb42cb2fca48
luanbrito7 <lab7@cin.ufpe.br>
2021-08-12 20:24:56 -0300

    add cadastro de cartao component

---
+++ front/src/paginas/CadastroCartao/index.js
@@ -0,0 +1,16 @@
+import api from "../../servicos/api"
+import { pegarToken } from "../../servicos/auth"
+import { CadastroCartaoView } from './view';
+
+export function CadastroCartaoController (props) {
+    async function cadastrarCartao(type, name, number, expiration, cvv) {
+        const res = await api.post(
+          '/cartoes/cadastrar',
+          { type, name, number, expiration, cvv }
+        )
+        console.log(res)
+        props.history.push("/cartoes")
+    }
+
+    return <CadastroCartaoView onSubmit={cadastrarCartao}/>
+}
