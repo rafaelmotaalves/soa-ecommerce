d18eb2cfff311a2d0056363fa70127b5e48077da
rafaelmotaalves <rma7@cin.ufpe.br>
2021-08-13 22:37:40 -0300

    add docker configurations

--- controle-acesso/config.py
+++ controle-acesso/config.py
@@ -1,5 +1,11 @@
+import os
+
+database_host = os.environ.get('DATABASE_HOST', 'localhost')
+database_port = os.environ.get('DATABASE_PORT', '15432')
+secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key')
+
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:15432/acesso'
+SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:password@{database_host}:{database_port}/acesso'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "teste_secret_key"
+SECRET_KEY = secret_key
